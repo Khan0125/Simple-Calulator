be6fcf85a4bdb71766a314ea02c92b26f53eefca
Khan0125 <hk6970172@gmail.com>
2023-04-02 22:54:12 +0500

    main file is added

--- Subtraction.py
+++ Subtraction.py
@@ -1,3 +1,5 @@
 def subtract(x,y) :
     return x-y
 
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+from Addition import Add
+from Subtraction import subtract
+from Multiply import multi
+
+
+if __name__=="__main__":
+    A=6
+    B=4
+
+
+    print(Add(A,B))
+    print(subtract(A,B))
+    print(multi(A,B))
+
